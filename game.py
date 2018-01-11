6105437c3b6982aa5af4289ec2a8c61097a3795e
jceterko <jceterko@ucvts.tec.nj.us>
2018-01-11 17:59:29 +0000

    verison 4? I think

--- game.py
+++ game.py
@@ -15,9 +15,9 @@ lucifer = Character("Lucifer", "Angel", "Narcissistic personality disorder...Ok,
 print("Welcome to Supernatural Trivia! You are going to answer questions in order to defeat your enemy. There will be 3 enemies, 3 stages and each one will get harder as you go. Now lets introduce our characters.")
 # printing introduction to game
 
-print(f"Here is {sam.name}. He is a {sam.species}. You have anything you would like to say {sam.name}? {sam.famous_quote}")
-print(f"Next up is {dean.name}. He is also a {dean.species}. And here he is! {dean.famous_quote}")
-print(f"Lastly, the beloved {castiel.name}. He is a {castiel.species}. OoOoOoOoO Ahhhhhhh. Anything you wanna say {castiel.name}? {castiel.famous_quote}")
+print(f"Here is {sam.name}. He is a {sam.species}. You have anything you would like to say {sam.name}? {sam.name}: {sam.famous_quote}")
+print(f"Next up is {dean.name}. He is also a {dean.species}. And here he is! {dean.name}: {dean.famous_quote}")
+print(f"Lastly, the beloved {castiel.name}. He is a {castiel.species}. OoOoOoOoO Ahhhhhhh. Anything you wanna say {castiel.name}? {castiel.name}: {castiel.famous_quote}")
 # printing character introductions
 
 character = input(f'Now what character will you control? {sam.name}, {dean.name} or {castiel.name}')
@@ -25,6 +25,23 @@ print(f'Great! You chose {character}. On we go to the first hunt.')
 # using input to determine player's character
 
 print("You are now in an abandoned barn in the middle of Kansas and you notice it is getting extremely cold inside depsite the fact that is summer. Then, suddenly, a figure appears in the darkness...")
-print(f'Well well well, look who we have here. You know {character}, I thought you were smarter than that and would stay away. You do know how I am right? For god\'s sake I am {crowley.name}, the {crowley.species}! {crowley.famous_quote}')
+print(f'{crowley.name}: Well well well, look who we have here. You know {character}, I thought you were smarter than that and would stay away. You do know how I am right? For god\'s sake I am {crowley.name}, the {crowley.species}! You see {crowley.famous_quote}')
+# introducint Stage 1
+
+print(f'{character}: Seems like times have changed for the worst there {crowley.name}.')
+print(f'{crowley.name}: You take that back! NO actually, I\'ll just kill ya instead!')
+print(f'{character}: oh sh*t')
+print(f'Ok if you somehow forgot the purpose of this game, you will be asked a series of 3 questions and you must get 1 of them right in order to escape and defeat {crowley.name}. Here we go!')
+# setting up battle questions
+
+questions1 = ['What is the last name of Sam and Dean?', 'Who is the older brother, Sam or Dean?', 'What creature killed Sam and Dean\'s mom?']
+questions2 = ['How many seasons of Supernatural are there?', 'What is the name of Sam and Dean\'s angel friend?', 'What creature can become the appearance of another person by shedding its skin?']
+questions3= ['In the pilot episode, who or what did Sam and Dean search for?', 'What color car does Dean drive?', 'What is Sam and Dean\'s dad\'s name?']
+answers1 = ['Winchester', 'Dean', 'Demon']
+answers2 = ['13', 'Castiel', 'shapeshifter']
+answers3 = ['The Woman in White', 'black', 'John']
+# creating question and answers list
+
+
 
 
